405603c4c0217d8b0e6e47c0fdaa7f8c43be9064
Ahmad Urabi <hamod9041@gmail.com>
2024-04-26 16:02:44 +0300

    Added Examples Section + Some Style Edits

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,29 @@ import { useState } from "react";
 import { Message } from "../models/message";
 const inter = Inter({ subsets: ["latin"] });
 
+import {motion} from "framer-motion";
+
 export default function Home() {
+
+  const examples = [
+    "What is the total amount of transactions?",
+    "What are the biggest transactions of the past month?",
+    "What is the average transaction amount in the past year?",
+  ]
+
+  const handleExampleClick = (e: any) => {
+    e.preventDefault();
+    const data = e.target.textContent;
+    let el = document.getElementById("text_input") as HTMLInputElement;
+    if (el) {
+      el.value = data;
+    }
+    let submitButton = document.querySelector("button[type='submit']") as HTMLButtonElement;
+    if (submitButton) {
+      submitButton.click();
+    }
+  }
+
   let [messages, setMessages] = useState<Message[]>([]);
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -13,10 +35,12 @@ export default function Home() {
 
     // Clear text field
     e.currentTarget["text_input"].value = "";
-    
-
+      
+    console.log(data)
     // Add User message to the chat
     setMessages([...messages, { sender: "User", message: data }]);
+    setMessages([...messages, { sender: "User", message: data }]);
+    setMessages([...messages, { sender: "User", message: data as string }]);
 
     // Add temporary message to the chat
     setMessages([...messages, { sender: "Bot", message: "Thinking..." }]);
@@ -46,19 +70,40 @@ export default function Home() {
     >
       <div>
         <h1 className="text-4xl font-bold text-center mx-auto my-12">
-          Menadevs Accounting Chatbot
+          Power of AI Team
         </h1>
-        <div className="mx-auto w-[500px] h-[500px] bg-white rounded-lg shadow-lg p-6 overflow-scroll">
+        <div className="flex flex-row gap-12 w-fit mx-auto">
+        <motion.div 
+          initial={{ opacity: 0, y: 16 }}
+          whileInView={{ opacity: 1, y: 0}}
+          transition={{ duration: 0.5 }}
+          viewport={{ once: true }}
+          className="w-[500px] h-[500px] bg-white rounded-lg shadow-lg p-6 overflow-scroll">
           {messages.map((message, index) => (
               <div key={index} className="text-left">
                {message.sender}: {message.message}
               </div>
             ))}
+        </motion.div>
+        <motion.div 
+          initial={{ opacity: 0, y: 16 }}
+          whileInView={{ opacity: 1, y: 0}}
+          transition={{ duration: 0.5 }}
+          viewport={{ once: true }}
+          className="min-w-[200px] h-[500px] bg-white rounded-lg shadow-lg p-6 overflow-scroll">
+          <h1 className="text-2xl font-bold mb-2">Examples:</h1>
+          <ul>
+            {examples.map((example, index) => (
+              <li key={index} onClick={handleExampleClick} className="text-left bg-blue-600 hover:bg-blue-500 hover:-translate-y-[2px] hover:cursor-pointer transition-all ease-in-out text-white rounded-xl shadow-xl py-4 px-6 my-2">{example}</li>
+            ))}
+          </ul>
+        </motion.div>
         </div>
-        <form onSubmit={handleSubmit} className="w-fit mx-auto">
-          <label htmlFor="text_input" className="block text-left mt-6">Enter your input here:</label>
-          <input id="text_input" className="w-[500px] bg-white rounded-lg shadow-lg mt-2 mx-auto h-12 p-2">
-          </input>
+        <form id="chat_form" onSubmit={handleSubmit} className="w-fit mx-auto">
+          <label htmlFor="text_input" className="block text-left mt-6">Ask a question:</label>
+          <input id="text_input" className="w-[500px] bg-white rounded-lg shadow-lg mt-2 mx-auto h-12 p-2"/>
+            <button type="submit" className="bg-blue-500 text-white rounded-lg shadow-lg h-12 w-24 ml-2">Send</button>
+          
         </form>
       </div>
     </main>
