4d645e129d700e13791a4d968fd159b9ec345cae
Ahmad Urabi <hamod9041@gmail.com>
2024-04-26 19:27:25 +0300

    SQL Connection

--- src/pages/api/chatapi.ts
+++ src/pages/api/chatapi.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 const axios = require("axios");
+import { Client, sql } from "@vercel/postgres";
+
 type ResponseData = {
   message: string;
 };
@@ -8,9 +10,24 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
+    // fetch table names
+    let tables = await sql` SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE';`;
+    console.log(tables);
   if (req.method === "POST") {
     const data = { data: req.body };
     let sql = await generateSQL(data);
+    if (sql) {
+        let data = await fetchData(sql);
+        if (data.length != 0) {
+            res.status(200).json({ message: JSON.stringify(data) || 'No Response' });
+        } else {
+            res.status(200).json({ message: 'No matching data found' });
+        }
+    }
+
+    
+
+
     res.status(200).json({ message: sql || 'No Response' });
   } else {
     res.setHeader("Allow", ["POST"]);
@@ -25,19 +42,21 @@ async function generateSQL(input: any){
     "${input.data}"
 
     You are also given the following schema:
-    Table: Transactions
-    Columns: 
-    - id
-    - date
-    - account
-    - amount
-    - description
-
-    Table: Accounts
-    Columns:
-    - id
-    - name
-    - balance
+    TABLE Account (
+        account_id SERIAL PRIMARY KEY,
+        account_name TEXT,
+        account_balance DECIMAL
+    );
+    
+    TABLE Transaction (
+        trans_id SERIAL PRIMARY KEY,
+        trans_type TEXT,
+        trans_method TEXT,
+        trans_amount DECIMAL,
+        trans_date DATE,
+        from_acc INT REFERENCES Account(account_id),
+        to_acc INT REFERENCES Account(account_id)
+    );
 
     The following conditions are also true:
     The current date is april 26 2024
@@ -86,4 +105,20 @@ async function generateSQL(input: any){
         });
       });
     return ans;
+}
+
+
+// TODO handle query / insert / update / delete
+
+async function fetchData(mysql: string) {
+    mysql = mysql.replace("SQL:\n", "");
+    mysql = mysql.replace("\n", " ");
+    console.log(mysql);
+    try {
+    const { rows } = await sql.query(mysql);
+    return rows;
+    } catch (e) {
+        console.log(e);
+        return [];
+    }
 }
