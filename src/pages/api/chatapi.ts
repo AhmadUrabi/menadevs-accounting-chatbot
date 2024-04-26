ff3c38d5f9ba241c4cfec0abb4e03689ed9a2a2d
Ahmad Urabi <hamod9041@gmail.com>
2024-04-26 14:57:23 +0300

    Basic GPT 3.5 Chatbot Functionality

--- src/pages/api/chatapi.ts
+++ src/pages/api/chatapi.ts
@@ -1,30 +1,48 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
- 
+import type { NextApiRequest, NextApiResponse } from "next";
+const axios = require("axios");
 type ResponseData = {
-  message: string
-}
- 
+  message: string;
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-    if (req.method === 'POST') {
-        const data = req.body.text;
-        const myreq = fetch("https://platform-api.aixplain.com/assets/pipeline/execution/run/662b8074d532f27117d5bb0d", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "x-api-key": process.env.AIXPLAIN_API_KEY || ''
-            },
-            body: JSON.stringify({
-                "data" : {
-                    "data": data
-                }
-            })
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            res.status(200).json({ message: data });
-        })
-    }
+  if (req.method === "POST") {
+    const data = { data: req.body };
+    const axres = await axios({
+      method: "post",
+      url: "https://models.aixplain.com/api/v1/execute/640b517694bf816d35a59125",
+      data: data,
+      headers: {
+        "x-api-key": process.env.AIXPLAIN_API_KEY,
+        "content-type": "application/json",
+      },
+    }).then(async (myres: any) => {
+      const urlToPoll = myres.data.data;
+      return await new Promise((resolve, reject) => {
+        const interval = setInterval(async () => {
+          try {
+            console.log("Polling");
+            const statusResponse = await axios({
+              method: "get",
+              url: urlToPoll,
+              headers: {
+                "x-api-key": process.env.AIXPLAIN_API_KEY,
+                "content-type": "application/json",
+              },
+            });
+            if (statusResponse.data.completed) {
+              clearInterval(interval);
+              res.status(200).json({ message: statusResponse.data.data });
+              resolve(statusResponse.data.data);
+            }
+          } catch (error) {
+            clearInterval(interval);
+            reject(error);
+          }
+        }, 1000);
+      });
+    });
+  }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,41 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import { useState } from "react";
-
+import { Message } from "../models/message";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  let [messages, setMessages] = useState<string[]>([]);
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  let [messages, setMessages] = useState<Message[]>([]);
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
+    // Get the data from the form
     const data = e.currentTarget["text_input"].value;
 
-    const res = await fetch("/api/chatapi", {
+    // Clear text field
+    e.currentTarget["text_input"].value = "";
+    
+
+    // Add User message to the chat
+    setMessages([...messages, { sender: "User", message: data }]);
+
+    // Add temporary message to the chat
+    setMessages([...messages, { sender: "Bot", message: "Thinking..." }]);
+
+    // Send the data to the server
+    const res = fetch("/api/chatapi", {
       method: "POST",
-      body: data}).then((res) => res.json());
-      console.log(res.message);
+      body: data}).then((res) => res.json()).then((res) => {
       if (!res.message.error) {
-      setMessages([...messages, res.message]);
+        console.log(res.message);
+        
+        // Clear temporary message
+        setMessages(messages.slice(0, messages.length - 1));
+
+        var newMsg = { sender: "Bot", message: res.message };
+        setMessages([...messages, newMsg]);
       }
+    }
+      );
     console.log("Form submitted");
   }
 
@@ -30,16 +48,16 @@ export default function Home() {
         <h1 className="text-4xl font-bold text-center mx-auto my-12">
           Menadevs Accounting Chatbot
         </h1>
-        <div className="mx-auto w-[500px] h-[500px] bg-white rounded-lg shadow-lg p-6">
+        <div className="mx-auto w-[500px] h-[500px] bg-white rounded-lg shadow-lg p-6 overflow-scroll">
           {messages.map((message, index) => (
-            <div key={index} className="text-left">
-              {message}
+              <div key={index} className="text-left">
+               {message.sender}: {message.message}
               </div>
             ))}
         </div>
         <form onSubmit={handleSubmit} className="w-fit mx-auto">
           <label htmlFor="text_input" className="block text-left mt-6">Enter your input here:</label>
-          <input id="text_input" className="w-[500px] bg-white rounded-lg shadow-lg mt-6 mx-auto h-12 p-2">
+          <input id="text_input" className="w-[500px] bg-white rounded-lg shadow-lg mt-2 mx-auto h-12 p-2">
           </input>
         </form>
       </div>
